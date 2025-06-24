209e859b854dfa986818186c51a75f225ed02bb9
phaothu2002x <truongthuan21342@gmail.com>
2025-06-24 15:25:10 +0700

    navbar floating & music button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './components/hero';
 import About from './components/About';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar />
             <Hero />
             <About />
             <section className="min-h-screen z-0 bg-blue-500" />
